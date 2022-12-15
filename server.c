dfec7cdfe42071f6fe6d88a2818280c5dd8b3070
Lukas Nehrke <lukasnehrke@protonmail.com>
2022-12-15 10:06:48 +0000

    feat: add server socket

--- server.c
+++ server.c
@@ -1,5 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+static void die(const char *message) {
+    perror(message);
+    exit(EXIT_FAILURE);
+}
+
+static void handleConnection() {}
 
 int main(void) {
-  exit(EXIT_SUCCESS);
+    struct sockaddr_in6 name = {
+        .sin6_family = AF_INET6,
+        .sin6_port   = htons(8080),
+        .sin6_addr   = in6addr_any,
+    };
+
+    int sock = socket(AF_INET6, SOCK_STREAM, 0);
+    if (sock == -1) die("socket");
+
+    if (bind(sock, (struct sockaddr *) &name, sizeof(name))) die("bind");
+	  if (listen(sock, SOMAXCONN)) die("listen");
+
+    while (1) {
+        int client = accept(sock, NULL, NULL);
+        if (client == -1) die("accept");
+
+        handleConnection(client);
+    }
 }
